a478a9d8dfc3494472ce068e7bc3561b74bbc389
pacdraw <105282232+pacdraw@users.noreply.github.com>
2022-05-10 17:42:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+introTitle: 'ebe à!',
+    introDesc: Trên thế giới này có hơn 7 tỷ 1 người. Nhưng mà với anh, thì nụ cười của em mới là đẹp nhất,
+    btnIntro: 'hehee',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    question:'Trên trái đất này có 7 tỉ người nhưng sao em vẫn chọn anh ❤',
+    btnReply: 'Gửi cho anh ❤',
+    reply: 'Tại vì anh là siu nhân<3',
+    mess: 'Anh biết mà 🥰 Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Làm người yêu anh nhé.',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'https://www.facebook.com/thpac.04' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
